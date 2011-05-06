a0f46727da687419c3a798934935be82930a4fc6
Juan Hawa <juan.hawa@gmail.com>
2011-05-06 13:07:41 -0400

    Did some refactoring to the models

--- refactor-this/photo.rb
+++ refactor-this/photo.rb
@@ -1,5 +1,14 @@
 class Photo
+
+  def to_image_tag(options)
+    "this image"
+  end
+
   def to_s
     "photo.rb"
+  end
+
+  def to_image_link(options)
+    "this link"
   end
 end
--- refactor-this/user_profile.rb
+++ refactor-this/user_profile.rb
@@ -4,5 +4,32 @@ class UserProfile
   def has_valid_photo?
     user && user.photo && File.exist?(user.photo.to_s)
   end
+
+  def photo_image_tag(options = { :size => '48x48', :html => {} })
+    if has_valid_photo?
+      photo.to_image_tag(options)
+    else
+      get_default_photo(options)
+    end
+  end
+
+  def photo_link(options = { :size => '48x48', :html => {} })
+    if has_valid_photo?
+      photo.to_image_link(options)
+    else
+      get_default_photo_link(options)
+    end
+  end
+
+  private
+    def get_default_photo(options)
+      user.rep ? "user190x119.jpg" : "user#{options[:size]}.jpg"
+    end
+
+    def get_default_photo_link(options)
+      default_photo = get_default_photo(options)
+      # Add default photo to link tag
+      user.rep ? "default link 100x100" : "default link 100x100"
+    end
 end
 
